1819fcb4f1d8454ab8f1a2ca51a7e3bddd167d1e
(Student  C00301121) Tymoteusz Walichnowski <C00301121@setu.ie>
2023-11-17 09:49:16 +0000

    added print menu

--- converter/converter/converter.cpp
+++ converter/converter/converter.cpp
@@ -1,6 +1,30 @@
 //tymoteusz walichnowski
 
+#include <iostream>
+
+char printMenu(); //signing PrintMenu
+
 int main()
 {
+	char userChoice; //letter for user to pick conversion
+	userChoice = printMenu(); //only print the menu once
+
+
+
+
 	return 1;
+}
+
+char printMenu() //written by tymek
+{
+	char choice = ' '; //user input choice
+	std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+	std::cout << "[A] Degrees <---> Radians        [a]" << std::endl;
+	std::cout << "[B] Centimeters <---> Feet & Inches        [b]" << std::endl;
+	std::cout << "[C] Litres <---> Gallons        [c]" << std::endl;
+
+	std::cout << "[Q] Exit        [---]   Exit        [q]" << std::endl;
+	std::cout << "Enter your Choice : ";
+	std::cin >> choice;
+	return choice;
 }
