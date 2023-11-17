ba3b087e9968e6237142f9f0c5c59a06f6cbf44a
(Student  C00301545) Jasmin Suter <C00301545@setu.ie>
2023-11-17 09:51:03 +0000

    added secondary printMenu function

--- converter/converter/converter.cpp
+++ converter/converter/converter.cpp
@@ -1,7 +1,23 @@
 //tymoteusz walichnowski
 //jasmin suter
 
+#include <iostream>
+
 int main()
 {
 	return 1;
+}
+
+char printMenu() // written by jasmin
+{
+	char choice = ' '; // user input choice
+	std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert right to left\n\n" << std::endl;
+
+	std::cout << "[D] Mpg <---> L/100km [d]" << std::endl;
+	std::cout << "[E] Kilograms <---> Stone & Pounds [e]" << std::endl;
+	std::cout << "[F] Celcius <---> Fahrenheit [f]" << std::endl;
+	std::cout << "[Q] Exit <---> Exit [q]" << std::endl;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
 }
